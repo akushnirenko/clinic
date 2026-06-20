2f52318812907f6630b3a82190c8239ff91a646e
kushnirenko <kushnirenko@gmail.com>
2026-06-20 18:46:43 +0300

    Added vite.config.js and tailwind

--- web/vite.config.js
+++ web/vite.config.js
@@ -1,18 +1,33 @@
-import { fileURLToPath, URL } from 'node:url'
+ import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+export default defineConfig(({ mode }) => {
+  // Load env variables ( .env .env.development .env.production )
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    plugins: [
+      vue(),
+      vueDevTools(),
+      tailwindcss(),
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      },
     },
-  },
+    server: {
+      proxy: {
+        '/api': {
+          // FIX: If env.VITE_API_BASE_URL is relative "/api", fallback to localhost:8000 for safety
+          target: env.VITE_API_BASE_URL === '/api' ? 'http://localhost:8000' : env.VITE_API_BASE_URL,
+          changeOrigin: true,
+        },
+      },
+    },
+  }
 })
